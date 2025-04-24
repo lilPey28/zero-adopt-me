75302b177d8aa3a6c03ef11a9098f5bf430cbd1f
lilPey28 <leonzeuva28@gmail.com>
2025-04-24 17:55:48 +0200

    GUI

--- script.lua
+++ script.lua
@@ -1,1 +1,13 @@
-game:GetService("RunService"):Set3dRenderingEnabled(false)
+-- Lade Rayfield UI Library
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+-- Erstelle Fenster
+local Window = Rayfield:CreateWindow({
+    Name = "Zero GUI",
+    LoadingTitle = "Zero GUI loading...",
+    LoadingSubtitle = "welcomme!",
+    ConfigurationSaving = {
+        Enabled = false
+    },
+    KeySystem = false
+})
