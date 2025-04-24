51e8276d82bfce0cbfe8ccb0259802caf158c817
lilPey28 <leonzeuva28@gmail.com>
2025-04-24 17:51:27 +0200

    test

--- script.lua
+++ script.lua
@@ -1,13 +1,1 @@
--- Lade Rayfield UI Library
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
-
--- Erstelle Fenster
-local Window = Rayfield:CreateWindow({
-    Name = "ZCustom GUI",
-    LoadingTitle = "ZCustom GUI lädt...",
-    LoadingSubtitle = "Willkommen!",
-    ConfigurationSaving = {
-        Enabled = false
-    },
-    KeySystem = false
-})
+game:GetService("RunService"):Set3dRenderingEnabled(false)
